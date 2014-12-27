1f68e62b144b324dcec660f8c03c798d56044e59
Shariq Hashme <shariqhashme@gmail.com>
2014-12-26 23:17:50 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -76,7 +76,7 @@ int main (int argc, char** argv) {
   bitwise_not(black,black);
   imshow("black", black);
 
-  threshold(black, black, 40, 1, THRESH_BINARY);
+  threshold(black, black, 40, 1, THRESH_BINARY_INV);
   split(image, channel);
   channel[0] = channel[0].mul(black);
   channel[1] = channel[1].mul(black);
