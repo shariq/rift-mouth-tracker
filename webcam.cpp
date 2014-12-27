f4431eb41ea9911bf2b65bc0409e417862e835e2
Shariq Hashme <shariqhashme@gmail.com>
2014-12-26 23:21:06 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -1,4 +1,4 @@
-#include "webcam.hpp"
+f#include "webcam.hpp"
 
 using namespace cv;
 using namespace std;
@@ -74,7 +74,7 @@ int main (int argc, char** argv) {
   black = (channel[0] + channel[1] + channel[2])/3.0;
   equalizeHist(black, black);
   bitwise_not(black,black);
-  threshold(black, black, 210, 1, THRESH_BINARY_INV);
+  threshold(black, black, 210, 255, THRESH_BINARY_INV);
   imshow("black", black);
 
   split(image, channel);
