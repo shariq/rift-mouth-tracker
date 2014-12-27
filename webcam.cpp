aa806e21c443f536b2a1c79f3a1f2bc34a4dfa8a
Shariq Hashme <shariqhashme@gmail.com>
2014-12-27 12:49:04 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -71,9 +71,8 @@ int main (int argc, char** argv) {
   bitwise_not(blurred_gray, blurred_gray);
   threshold(blurred_gray, threshold_gray, 210, 1, THRESH_BINARY);
   imshow("threshold", threshold_gray);
-  imshow("lol", blurred_gray);
 
-  Mat mask = threshold_gray.mul(blurred_gray/2.0);
+  Mat mask = threshold_gray.mul(gray);
   imshow("mask", mask);
 
   Moments lol = moments(mask, 1);
