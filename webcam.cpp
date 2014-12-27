d276e19bf84cae11cf02a1e11506826f8a1e4459
Shariq Hashme <shariqhashme@gmail.com>
2014-12-27 00:25:32 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -66,9 +66,15 @@ int main (int argc, char** argv) {
 
   Mat black, blurred;
   split(image, channel);
-//  merge(channel, 3, black);
-//  blur(black, blurred, Size(width/4.5,height/9));
-//  split(blurred, channel);
+  black = (channel[0] + channel[1] + channel[2])/3.0;
+  equalizeHist(black, black);
+  blur(black, blurred, Size(width/9,height/18));
+  threshold(blurred, blurred, 220, 255, THRESH_BINARY);
+  imshow("lol", blurred);
+  waitKey(0);
+  merge(channel, 3, black);
+  blur(black, blurred, Size(width/4.5,height/9));
+  split(blurred, channel);
   black = (channel[0] + channel[1] + channel[2])/3.0;
   equalizeHist(black, black);
   bitwise_not(black,black);
