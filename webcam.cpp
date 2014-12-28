d478e6dd3cddd05c6edf3d28d2d4dc99d74e17f5
Shariq Hashme <shariqhashme@gmail.com>
2014-12-27 22:49:38 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -100,8 +100,7 @@ int main (int argc, char** argv) {
   morphologyEx(canny, canny, MORPH_OPEN, kernel);
   imshow("canny2", canny*255);
   waitKey(1);
-  Size kernelSmall((kwidth/2)+(1-((kwidth/2)%2)),(kheight/2)+(1-((kheight/2)%2)));
-  kernel = getStructuringElement(MORPH_ELLIPSE, kernelSmall);
+  Mat kernalSmall = getStructuringElement(MORPH_ELLIPSE, Size((kwidth/2)+(1-((kwidth/2)%2)),(kheight/2)+(1-((kheight/2)%2))));
   morphologyEx(canny, canny, MORPH_CLOSE, kernelSmall);
   imshow("canny3", canny*255);
   waitKey(1);
