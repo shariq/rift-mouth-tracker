2154d6b73876cb3cc6f3f0dd72c4d0984670cc7e
Shariq Hashme <shariqhashme@gmail.com>
2014-12-28 01:56:33 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -134,8 +134,8 @@ int main (int argc, char** argv) {
 //  imshow("dark mask", gray.mul(kindofdark));
 
   Mat mask = flow.mul(kindofdark).mul(canny);
-  morphFast(mask, 100, tracker1+11-(tracker1%2), 0, 1);
-  morphFast(mask, 100, tracker2+11-(tracker2%2), 0, 0);
+  morphFast(mask, 200, tracker1+7-(tracker1%2), 0, 1);
+  morphFast(mask, 200, tracker2+7-(tracker2%2), 0, 0);
 
   imshow("mask", gray.mul(mask));
 
