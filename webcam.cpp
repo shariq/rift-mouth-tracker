fd998b1001236556ac9e481b45dcb7117e575689
Shariq Hashme <shariqhashme@gmail.com>
2014-12-28 22:32:25 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -243,7 +243,7 @@ int main (int argc, char** argv) {
   }
   double minVal, maxVal;//ignore minVal, it'll be 0
   minMaxLoc(rectImage, &minVal, &maxVal);
-  threshold(rectImage, rectImage, maxVal-1, 1, THRESH_BINARY);
+  threshold(rectImage, rectImage, maxVal-2, 1, THRESH_BINARY);
   printf("max: %.f\n",maxVal);
   times[6] += getMilliseconds() - timenow;
   timenow = getMilliseconds();
