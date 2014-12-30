a83b7baf52ea83bf71ffa8bbaae81a446ca24343
Shariq Hashme <shariqhashme@gmail.com>
2014-12-30 15:54:27 -0500

    hacking

--- webcam.cpp
+++ webcam.cpp
@@ -239,7 +239,7 @@ int main (int argc, char** argv) {
 // if nothing moved in some place over the past frame,
 // and nothing was detected last frame, whatever is
 // detected now is more likely to be bogus
-
+/*
   acfg = acfg*0.25 + fg;
 
 /*  double min_val, max_val;
